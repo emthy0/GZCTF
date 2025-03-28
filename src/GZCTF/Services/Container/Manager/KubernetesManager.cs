6c434fcd64929acafc2838ed2284337b3944bec3
Kuranasaki@M3Max <ironpan10@hotmail.com>
2025-03-28 14:36:45 +0700

    EMERGENCY COMMIT: Earthquake backup Fri Mar 28 14:36:45 +07 2025

--- src/GZCTF/Services/Container/Manager/KubernetesManager.cs
+++ src/GZCTF/Services/Container/Manager/KubernetesManager.cs
@@ -218,6 +218,192 @@ public class KubernetesManager : IContainerManager
 
         return container;
     }
+    
+    public async Task<Models.Data.Container?> CreateVpnContainerAsync(ContainerConfig config,
+        CancellationToken token = default)
+    {
+        var imageName = "OpenVPN";
+        var authSecretName = _meta.AuthSecretName;
+        KubernetesConfig options = _meta.Config;
+
+        var chalImage = imageName.ToValidRFC1123String("chal");
+
+        var name = $"vpn-{config.TeamId}-{config.UserId.ToString()}-{Ulid.NewUlid().ToString().ToLowerInvariant()}";
+
+        var pod = new V1Pod("v1", "Pod")
+        {
+            Metadata = new V1ObjectMeta
+            {
+                Name = name,
+                NamespaceProperty = options.Namespace,
+                Labels = new Dictionary<string, string>
+                {
+                    ["ctf.gzti.me/ResourceId"] = name,
+                    ["ctf.gzti.me/Image"] = chalImage,
+                    ["ctf.gzti.me/TeamId"] = config.TeamId,
+                    ["ctf.gzti.me/UserId"] = config.UserId.ToString(),
+                    ["ctf.gzti.me/ChallengeId"] = "VPN"
+                }
+            },
+            Spec = new V1PodSpec
+            {
+                ImagePullSecrets =
+                    authSecretName is null
+                        ? Array.Empty<V1LocalObjectReference>()
+                        : new List<V1LocalObjectReference> { new() { Name = authSecretName } },
+                DnsPolicy = "None",
+                DnsConfig = new() { Nameservers = options.Dns ?? ["8.8.8.8", "223.5.5.5", "114.114.114.114"] },
+                EnableServiceLinks = false,
+                Containers =
+                [
+                    new V1Container
+                    {
+                        Name = name,
+                        Image = config.Image,
+                        ImagePullPolicy = "Always",
+                        Env =
+                            config.Flag is null
+                                ? [new V1EnvVar("GZCTF_TEAM_ID", config.TeamId)]
+                                :
+                                [
+                                    new V1EnvVar("GZCTF_FLAG", config.Flag),
+                                    new V1EnvVar("GZCTF_TEAM_ID", config.TeamId)
+                                ],
+                        Ports = [new V1ContainerPort(config.ExposedPort)],
+                        Resources = new V1ResourceRequirements
+                        {
+                            Limits = new Dictionary<string, ResourceQuantity>
+                            {
+                                ["cpu"] = new($"{config.CPUCount * 100}m"),
+                                ["memory"] = new($"{config.MemoryLimit}Mi"),
+                                ["ephemeral-storage"] = new($"{config.StorageLimit}Mi")
+                            },
+                            Requests = new Dictionary<string, ResourceQuantity>
+                            {
+                                ["cpu"] = new("10m"),
+                                ["memory"] = new("32Mi")
+                            }
+                        }
+                    }
+                ],
+                RestartPolicy = "Never",
+                AutomountServiceAccountToken = false
+            }
+        };
+
+        try
+        {
+            pod = await _client.CreateNamespacedPodAsync(pod, options.Namespace, cancellationToken: token);
+        }
+        catch (HttpOperationException e)
+        {
+            _logger.SystemLog(
+                Program.StaticLocalizer[nameof(Resources.Program.ContainerManager_ContainerCreationFailedStatus), name,
+                    e.Response.StatusCode],
+                TaskStatus.Failed, LogLevel.Warning);
+            _logger.SystemLog(
+                Program.StaticLocalizer[nameof(Resources.Program.ContainerManager_ContainerCreationFailedResponse),
+                    name,
+                    e.Response.Content],
+                TaskStatus.Failed, LogLevel.Error);
+            return null;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e,
+                Program.StaticLocalizer[nameof(Resources.Program.ContainerManager_ContainerCreationFailed), name]);
+            return null;
+        }
+
+        if (pod is null)
+        {
+            _logger.SystemLog(
+                Program.StaticLocalizer[nameof(Resources.Program.ContainerManager_ContainerInstanceCreationFailed),
+                    config.Image.Split("/").LastOrDefault() ?? ""], TaskStatus.Failed,
+                LogLevel.Warning);
+            return null;
+        }
+
+        // Service is needed for port mapping
+        var service = new V1Service("v1", "Service")
+        {
+            Metadata = new V1ObjectMeta
+            {
+                Name = name,
+                NamespaceProperty = _meta.Config.Namespace,
+                Labels = new Dictionary<string, string> { ["ctf.gzti.me/ResourceId"] = name }
+            },
+            Spec = new V1ServiceSpec
+            {
+                Type = _meta.ExposePort ? "NodePort" : "ClusterIP",
+                Ports = [new V1ServicePort(config.ExposedPort, targetPort: config.ExposedPort)],
+                Selector = new Dictionary<string, string> { ["ctf.gzti.me/ResourceId"] = name }
+            }
+        };
+
+        try
+        {
+            service = await _client.CoreV1.CreateNamespacedServiceAsync(service, _meta.Config.Namespace,
+                cancellationToken: token);
+        }
+        catch (HttpOperationException e)
+        {
+            try
+            {
+                // remove the pod if service creation failed, ignore the error
+                await _client.CoreV1.DeleteNamespacedPodAsync(name, _meta.Config.Namespace, cancellationToken: token);
+            }
+            catch
+            {
+                // ignored
+            }
+
+            _logger.SystemLog(
+                Program.StaticLocalizer[nameof(Resources.Program.ContainerManager_ServiceCreationFailedStatus), name,
+                    e.Response.StatusCode],
+                TaskStatus.Failed, LogLevel.Warning);
+            _logger.SystemLog(
+                Program.StaticLocalizer[nameof(Resources.Program.ContainerManager_ServiceCreationFailedResponse), name,
+                    e.Response.Content],
+                TaskStatus.Failed, LogLevel.Error);
+            return null;
+        }
+        catch (Exception e)
+        {
+            try
+            {
+                // remove the pod if service creation failed, ignore the error
+                await _client.CoreV1.DeleteNamespacedPodAsync(name, _meta.Config.Namespace, cancellationToken: token);
+            }
+            catch
+            {
+                // ignored
+            }
+
+            _logger.LogError(e,
+                Program.StaticLocalizer[nameof(Resources.Program.ContainerManager_ServiceCreationFailed), name]);
+            return null;
+        }
+
+        var container = new Models.Data.Container
+        {
+            ContainerId = name,
+            Image = config.Image,
+            Port = config.ExposedPort,
+            IP = service.Spec.ClusterIP,
+            IsProxy = !_meta.ExposePort,
+            // No tracking for k8s-managed containers
+            Status = ContainerStatus.Running
+        };
+
+        if (!_meta.ExposePort)
+            return container;
+
+        container.PublicIP = _meta.PublicEntry;
+        container.PublicPort = service.Spec.Ports[0].NodePort;
+
+        return container;
+    }
 
     public async Task DestroyContainerAsync(Models.Data.Container container, CancellationToken token = default)
     {
